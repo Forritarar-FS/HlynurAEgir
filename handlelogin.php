3e63990ee59d7429e3e91ad064036cffb71acc2f
unknown <fs417450@stofa-101-05.fss.is>
2015-10-27 15:04:19 +0000

    added handlelogin

---
+++ handlelogin.php
@@ -0,0 +1,20 @@
+
+<?php
+if
+mysql_connect("localhost", "root", "")or die("cannot connect");
+mysql_select_db("lokaverk")or die("cannot select DB");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lokaverk";
+
+$sqlQuery= "Select Username from login";
+
+$Username =
+$Password =
+$Email =
+
+
+
+ ?>
